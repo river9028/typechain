70274fcb59d91a42d548dbcda518540f58afb87b
DESKTOP-NQTKL8R\river k <river9028@gmail.com>
2021-02-04 19:22:24 +0900

    Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
+	// sayHello() => return "hello"
+	static calculateBlockHash = (index: number, previouseHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
+
+	static validateStructure = (aBlock: Block): boolean =>
+		typeof aBlock.index === "number" &&
+		typeof aBlock.hash === "string" &&
+		typeof aBlock.previouseHash === "string" &&
+		typeof aBlock.timestamp === "number" &&
+		typeof aBlock.data === "string";
+
 	public index: number;
 	public hash: string;
 	public previouseHash: string;
 	public data: string;
 	public timestamp: number;
-
-	// sayHello() => return "hello"
-	static calculateBlockHash = (index: number, previouseHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
 
 	constructor(index: number, hash: string, previouseHash: string, data: string, timestamp: number) {
 		this.index = index;
@@ -32,14 +39,21 @@ const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 const createNewBlock = (data: string): Block => {
 	const previousBlock: Block = getLatestBlock();
 	const newIndex: number = previousBlock.index + 1;
-	const newTimeStamp : number = getNewTimestamp();
-	const newHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
+	const newTimeStamp: number = getNewTimestamp();
+	const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
 
 	const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
-	
+
 	return newBlock;
 };
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
-
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+	if (!Block.validateStructure(candidateBlock)) {
+		return false;
+	} else if (previousBlock.index + 1 !== candidateBlock.index) {
+		return false;
+	} else if (previousBlock.hash + 1 !== candidateBlock.previouseHash) {
+		return false;
+	} eles if()
+}
 export { };
